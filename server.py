e8f25ea43d76f3ef11aea3196b8b00074b17e0e5
Kalyb Prince <kalybprince91@gmail.com>
2021-08-07 10:02:24 -1000

    added session

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect
+from flask.globals import session
+
 app = Flask(__name__)  
+app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')         
 def index():
@@ -7,12 +10,13 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    session['total'] = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple']) + int(request.form['blackberry'])
+    print(f"Charging {request.form['first_name']} for {session['total']} fruits")
+    return render_template("checkout.html", info=request.form)
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
 
 if __name__=="__main__":   
-    app.run(debug=True)    
+    app.run(debug=True)
